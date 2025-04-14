47278a2d403aec558742cc61165347893ead644b
Lucas Daniel Souza de Medeiros <lucasddaniel@gmail.com>
2025-04-14 14:29:53 -0300

    adjustments of menu activities

---
+++ js/translations.js
@@ -0,0 +1,14 @@
+const translations = {
+  welcome: {
+    es: "Bienvenidos",
+    en: "Welcome",
+    pt: "Bem-vindos"
+  },
+  "welcome-text": {
+    es: "Ser parte de la familia de Dios es una nueva vida y vida eterna. Te esperamos...",
+    en: "Being part of God's family is a new life and eternal life. We are waiting for you...",
+    pt: "Ser parte da família de Deus é uma nova vida e vida eterna. Estamos esperando por você..."
+  }
+};
+
+export default translations; 
